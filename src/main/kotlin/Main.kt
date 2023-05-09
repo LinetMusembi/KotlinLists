dfc391e9dd9dd603be6ff00b23c2ac08c5a5756d
linetmusembi <musembilinetmwongeli@gmail.com>
2023-05-09 23:55:27 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main() {
     checkNums()
+    println( names("Lynet","Irene","Emmily").contentToString())
 
 
 }
@@ -13,4 +14,21 @@ fun checkNums(){
     var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
     var sum = numbers[1]+numbers[4]
     println(sum)
+    println(numbers.indexOf(158))
+    println(numbers.sorted())
+}
+//2. Given a list of Person objects, each with the attributes, name, age, height, and weight.
+//Sort the list in order of descending age (2 points)
+
+//3. Create a function that takes in 3 names and returns a string array containing all 3 names.
+fun names(name1:String,name2:String,name3:String):Array<String>{
+    var friends = arrayOf(name1,name2,name3)
+    return friends
+
+}
+//4. Write a function that takes in a list of Car objects each with a registration and mileage attribute
+//and returns the average mileage of all the vehicles in the list.
+
+fun vehicle(){
+
 }
